a644ff191c73bf4b8345ec0f5bfaf734de235ef4
QuanWa89 <qw.drylab@gmail.com>
2023-01-18 04:06:48 -0500

    better

--- scripts/batch.sh
+++ scripts/batch.sh
@@ -1,5 +1,7 @@
 csv2json -d -t $GITHUB_WORKSPACE/inst/curated/AsnicarF_2021_metadata.tsv test.json
 sed -i '1s/^/{"samples":/' test.json
 echo '}' >> test.json
+head test.json
+tail test.json
 linkml-validate -s schema.yaml test.json > results.txt
 cat results.txt
