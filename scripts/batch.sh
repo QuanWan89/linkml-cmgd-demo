b0739e1aa92a466628cedd37e5e0ff6f011ad626
QuanWa89 <qw.drylab@gmail.com>
2023-01-18 04:10:06 -0500

    what

--- scripts/batch.sh
+++ scripts/batch.sh
@@ -1,4 +1,4 @@
-csv2json -d -t $GITHUB_WORKSPACE/inst/curated/AsnicarF_2021_metadata.tsv test.json
+csv2json -d -t ../inst/curated/AsnicarF_2021_metadata.tsv test.json
 sed -i '1s/^/{"samples":/' test.json
 echo '}' >> test.json
 head test.json
